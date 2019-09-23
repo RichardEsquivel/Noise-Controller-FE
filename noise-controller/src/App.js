b4ae00c3fc31abd7f9b8c987e72aa57697e6bfe4
jambis <imjaamees@gmail.com>
2019-09-23 12:29:07 -0500

    Completed general routing of the website

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Creatures from "./Components/Creatures";
+import Settings from "./Components/Settings";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return (
     <div>
-      hello World
+      <Header />
+      <Route exact path="/" component={Creatures} />
+      <Route path="/settings" component={Settings} />
+      <Route path="/signup" component={SignUp} />
     </div>
   );
 }
