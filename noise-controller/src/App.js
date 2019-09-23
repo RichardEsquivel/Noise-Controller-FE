19d14e8c5609d5e9e0bd9d3272b7edda220a5a28
RichardEsquivel <rlesquivel@gmail.com>
2019-09-23 13:35:35 -0500

    setting up basic login fields and Routes in App.Js may modifiy those depending on what has been commited to Routing already

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route, Link, Redirect } from 'react-router-dom';
+import SignUp from "./Components/SignUp";
+
 
 function App() {
-  return (
-    <div>
-      hello World
-    </div>
-  );
+	return (
+		<div>
+			hello World
+	  <Route exact path="/login" component={SignUp} />
+
+
+		</div>
+	);
 }
 
 export default App;
