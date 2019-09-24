6ad18391216f5e75e28ab661d5ecec13cb580358
RichardEsquivel <rlesquivel@gmail.com>
2019-09-24 14:36:51 -0500

    Pushing for edits

--- noise-controller/src/Components/Creatures.js
+++ noise-controller/src/Components/Creatures.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import VolumeSlider from './VolumeSlider';
 
-function Creatures() {
-  return (
-    <div>
-      Creatures
-    </div>
-  );
+import VolumeAnalyser from './VolumeAnalyser'
+
+class Creatures extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			audio: null
+		}
+	}
+
+	getMicrophone = async () => {
+		const audio = await navigator.mediaDevices.getUserMedia({
+			audio: true,
+			video: false
+		})
+		this.setState({ audio })
+	}
+
+	stopMicrophone = () => {
+		this.state.audio.getTracks().forEach(track => track.stop());
+		this.setState({ audio: null });
+	}
+
+	toggleMicrophone = () => {
+		if (this.state.audio) {
+			this.stopMicrophone();
+		} else {
+			this.getMicrophone();
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.toggleMicrophone}>{this.state.audio ? 'Stop Mic' : 'Start Mic'}</button>
+				{this.state.audio &&
+					<div>
+						<VolumeAnalyser audio={this.state.audio} />
+					</div>
+				}
+			</div>
+		);
+	}
 }
 
 export default Creatures;
--- noise-controller/src/Components/VolumeSlider.js
+++ noise-controller/src/Components/VolumeSlider.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function VolumeSlider() {
-  return (
-    <div>
-      Volume Slider
-    </div>
-  );
+class VolumeSlider extends React.Component {
+	constructor(props) {
+		super(props);
+		this.canvas = React.createRef();
+	}
+
+	render() {
+		return <canvas width="280" height="280" ref={this.canvas} />;
+	}
 }
 
 export default VolumeSlider;
