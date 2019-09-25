1a0723c86ed2c3130ff6ea458e3f7e7058bc5572
RichardEsquivel <rlesquivel@gmail.com>
2019-09-25 18:21:52 -0500

    Added resource needed for module

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -5,43 +5,42 @@ import Creatures from "./Components/Creatures";
 import Settings from "./Components/Settings";
 import SignUp from "./Components/SignUp";
 import Login from "./Components/Login";
-import SignUp from "./Components/SignUp.js";
 import "./App.css";
 
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        if (localStorage.getItem("token")) {
-          return <Component {...props} />;
-        } else {
-          return <Redirect to="/login" />;
-        }
-      }}
-    />
-  );
+	return (
+		<Route
+			{...rest}
+			render={props => {
+				if (localStorage.getItem("token")) {
+					return <Component {...props} />;
+				} else {
+					return <Redirect to="/login" />;
+				}
+			}}
+		/>
+	);
 };
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(false);
 
-  return (
-    <div>
-      <Header loggedIn={loggedIn} />
-      <Route
-        path="/login"
-        render={props => <Login {...props} setLoggedIn={setLoggedIn} />}
-      />
-      <Route path="/creatures" component={Creatures} />
-      {/* 
+	return (
+		<div>
+			<Header loggedIn={loggedIn} />
+			<Route
+				path="/login"
+				render={props => <Login {...props} setLoggedIn={setLoggedIn} />}
+			/>
+			<Route path="/creatures" component={Creatures} />
+			{/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
 		*/}
-      <ProtectedRoute path="/settings" component={Settings} />
-      <Route path="/signup" component={SignUp} />
-    </div>
-  );
+			<ProtectedRoute path="/settings" component={Settings} />
+			<Route path="/signup" component={SignUp} />
+		</div>
+	);
 
 }
 
