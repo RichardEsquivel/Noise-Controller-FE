a77bff41ef59ca1c49fcdeeb79723a12786206ad
jambis <imjaamees@gmail.com>
2019-09-26 11:47:56 -0500

    Added scoreboard modal

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -1,45 +1,49 @@
 import React, { useState } from "react";
 import { Route, Redirect } from "react-router-dom";
+import { ModalContainer } from "react-router-modal";
 import Header from "./Components/Header";
 import Creatures from "./Components/Creatures";
 import Settings from "./Components/Settings";
 import SignUp from "./Components/SignUp";
 import Login from "./Components/Login";
+import "react-router-modal/css/react-router-modal.css";
 import "./App.css";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-	return (
-		<Route
-			{...rest}
-			render={props => {
-				if (localStorage.getItem("token")) {
-					return <Component {...props} />;
-				} else {
-					return <Redirect to="/login" />;
-				}
-			}}
-		/>
-	);
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (localStorage.getItem("token")) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+  );
 };
-function App() {
-	const [loggedIn, setLoggedIn] = useState(false);
+function App(props) {
+  const [loggedIn, setLoggedIn] = useState(false);
+  console.log(props);
+  return (
+    <div>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <ModalContainer />
 
-	return (
-		<div>
-			<Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-			<Route
-				path="/login"
-				render={props => <Login {...props} setLoggedIn={setLoggedIn} />}
-			/>
-			<Route path="/creatures" component={Creatures} />
-			{/* 
+      <Route
+        path="/login"
+        render={props => <Login {...props} setLoggedIn={setLoggedIn} />}
+      />
+      <Route path="/creatures" component={Creatures} />
+      {/* 
           Build a PrivateRoute component that will 
           display Settings when you're authenticated 
 		*/}
-			<ProtectedRoute path="/settings" component={Settings} />
-			<Route path="/signup" component={SignUp} />
-		</div>
-	);
+      <ProtectedRoute path="/settings" component={Settings} />
+      <Route path="/signup" component={SignUp} />
+    </div>
+  );
 }
 
 export default App;
